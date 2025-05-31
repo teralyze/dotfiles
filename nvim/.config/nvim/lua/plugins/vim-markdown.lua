4c8bfacadebf2952b0d52a56220f3b291cd31a9d
teralyze <teralyze@gmail.com>
2025-05-31 22:20:20 +0300

    fixed neovim

--- nvim/.config/nvim/lua/plugins/vim-markdown.lua
+++ nvim/.config/nvim/lua/plugins/vim-markdown.lua
@@ -1,8 +1,53 @@
 return {
-	'MeanderingProgrammer/render-markdown.nvim',
+	"MeanderingProgrammer/render-markdown.nvim",
 	opts = {},
-	dependencies = { 
-		'nvim-treesitter/nvim-treesitter',
-		'nvim-tree/nvim-web-devicons' 
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
 	},
+	config = function()
+		require("render-markdown").setup({
+			heading = {
+				sign = false,
+				position = "inline",
+				width = "block",
+				left_margin = 0.5,
+				left_pad = 0.2,
+				right_pad = 0.2,
+			},
+			link = {
+				enabled = true,
+				render_modes = false,
+				footnote = {
+					enabled = true,
+					superscript = true,
+					prefix = "",
+					suffix = "",
+				},
+				image = "󰥶 ",
+				email = "󰀓 ",
+				hyperlink = "󰌹 ",
+				highlight = "RenderMarkdownLink",
+				wiki = {
+					icon = "󱗖 ",
+					body = function()
+						return nil
+					end,
+					highlight = "RenderMarkdownWikiLink",
+				},
+				custom = {
+					web = { pattern = "^http", icon = "󰖟 " },
+					discord = { pattern = "discord%.com", icon = "󰙯 " },
+					github = { pattern = "github%.com", icon = "󰊤 " },
+					gitlab = { pattern = "gitlab%.com", icon = "󰮠 " },
+					google = { pattern = "google%.com", icon = "󰊭 " },
+					neovim = { pattern = "neovim%.io", icon = " " },
+					reddit = { pattern = "reddit%.com", icon = "󰑍 " },
+					stackoverflow = { pattern = "stackoverflow%.com", icon = "󰓌 " },
+					wikipedia = { pattern = "wikipedia%.org", icon = "󰖬 " },
+					youtube = { pattern = "youtube%.com", icon = "󰗃 " },
+				},
+			},
+		})
+	end,
 }
